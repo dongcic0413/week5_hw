f18a081e31f455ea56da9050891adc9f97195759
dongcic0413 <dongcic0413@gmail.com>
2026-09-15 17:55:40 +0900

    임수현 신병받아라

---
+++ accounts/views.py
@@ -0,0 +1,20 @@
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+
+# Create your views here.
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("post_list")
+
+    else:
+        form = SignUpForm()
+
+    return render(
+        request, "accounts/signup.html", {"form": form}
+    )
--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "post_list"
+LOGOUT_REDIRECT_URL = "post_list"
